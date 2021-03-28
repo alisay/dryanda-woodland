631fd9e06713eebcd0bb6fc7f6c38cc17d75abaa
Alisa <alisa.blakeney@gmail.com>
2021-03-28 13:48:40 +1100

    configure cors to allow requests from netlify app

--- dryanda.js
+++ dryanda.js
@@ -1,26 +1,32 @@
 // Imports the Google Cloud client library
-const {Storage} = require('@google-cloud/storage');
+import { Storage } from '@google-cloud/storage';
 
 // Creates a client
 const storage = new Storage();
-
 const bucketName = 'dryandra-woodland';
 
-// The origin for this CORS config to allow requests from
-// const origin = 'http://example.appspot.com';
+// CORS config
+const origin = 'https://dryandra.netlify.app/';
+const responseHeader = 'Content-Type';
+const maxAgeSeconds = 3600;
+const method = 'GET';
 
-// The response header to share across origins
-// const responseHeader = 'Content-Type';
-
-// The maximum amount of time the browser can make requests before it must
-// repeat preflighted requests
-// const maxAgeSeconds = 3600;
-
-// The name of the method
-// See the HttpMethod documentation for other HTTP methods available:
-// https://cloud.google.com/appengine/docs/standard/java/javadoc/com/google/appengine/api/urlfetch/HTTPMethod
-// const method = 'GET';
-
+async function configureBucketCors() {
+    await storage.bucket(bucketName).setCorsConfiguration([
+      {
+        maxAgeSeconds,
+        method: [method],
+        origin: [origin],
+        responseHeader: [responseHeader],
+      },
+    ]);
+  
+    console.log(`Bucket ${bucketName} was updated with a CORS config
+        to allow ${method} requests from ${origin} sharing 
+        ${responseHeader} responses across origins`);
+  }
+  
+  configureBucketCors().catch(console.error);
 
 
 // for cross browser compatibility
